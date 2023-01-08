3f7e6a1b528bb53d1412c46d3b9ef18e275b03d1
chemf <eoyohe@gmail.com>
2023-01-08 14:43:40 +0800

    fix: update assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/2048-vue',
+    assetsPublicPath: '/2048',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
